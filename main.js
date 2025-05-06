3e542b57af242579ee8fa267b80e7fb7ba744af6
Emily Gronowski <emilygronowski3@gmail.com>
2025-05-06 18:39:15 -0400

    Connect JavaScript to HTML

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-const buildMaker = document.getElementById('buildmaker');
-const randomize = document.querySelector('.randomize');
-const build = document.querySelector('.build');
-
 function randomValueFromArray(array) {
     const random = Math.floor(Math.random()*array.length);
     return array[random];
@@ -11,28 +7,34 @@ const insertCharacter = ["Mario", "Luigi", "Peach", "Daisy", "Rosalina", "Tanook
 const insertKart = ["Standard Kart"]; 
 const insertTires = ["Standard"];
 const insertGlider = ["Super Glider"];
-const buildText = `Character: :insertCharacter: Kart: :insertKart: Tires: :insertTires: Glider: :insertGlider:`;
-
 
 const myButton = document.getElementById("myButton");
 myButton.addEventListener("click", function() {
     result()
-    });
+});
 
 function result() {
-    let newBuild = buildText;
+    const characterInsert = randomValueFromArray(insertCharacter);
+    console.debug(`Character: ${characterInsert}`);
 
-    const characterInsert = randomValueFromArray(insertCharacter);  
     const kartInsert = randomValueFromArray(insertKart);
+    console.debug(`Kart: ${kartInsert}`);
+
     const tiresInsert = randomValueFromArray(insertTires);
+    console.debug(`Wheels: ${tiresInsert}`);
+
     const gliderInsert = randomValueFromArray(insertGlider);
+    console.debug(`Glider: ${gliderInsert}`);
 
-    newBuild = newBuild
-            .replace(":insertCharacter:", characterInsert)
-            .replace(":insertKart:", kartInsert)
-            .replace(":insertTires:", tiresInsert)
-            .replace(":insertGlider:", gliderInsert)
+    const characterElement = document.getElementById("character");
+    characterElement.innerHTML = characterInsert;
 
-    console.log(newBuild)
-    return newBuild;
+    const kartElement = document.getElementById("kart");
+    kartElement.innerHTML = kartInsert;
+
+    const tiresElement = document.getElementById("tires");
+    tiresElement.innerHTML = tiresInsert;
+
+    const gliderElement = document.getElementById("glider");
+    gliderElement.innerHTML = gliderInsert;
 }
