5f38027b0cc08a0b4d4fe994d7e1127382cc676f
Emily Gronowski <emilygronowski3@gmail.com>
2025-05-07 19:13:05 -0400

    Add all character options

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ function randomValueFromArray(array) {
     return array[random];
 }
 
-const insertCharacter = ["Mario"];
+const insertCharacter = ["Mario", "Luigi", "Peach", "Daisy", "Rosalina", "Tanooki Mario",
+                        "Cat Peach", "Birdo", "Birdo (Light Blue)", "Birdo (Black)", 
+                        "Birdo (Red)", "Birdo (Yellow)", "Birdo (White)", "Birdo (Blue)",
+                        "Birdo (Green)", "Birdo (Orange)", "Yoshi", "Light-blue Yoshi",
+                        "Black Yoshi", "Red Yoshi", "Yellow Yoshi", "White Yoshi", "Blue Yoshi",
+                        "Pink Yoshi", "Orange Yoshi", "Toad", "Koopa Troopa", "Shy Guy", 
+                        "Light-blue Shy Guy", "Black Shy Guy", "Green Shy Guy", "Yellow Shy Guy",
+                        "White Shy Guy", "Blue Shy Guy", "Pink Shy Guy", "Orange Shy Guy", "Lakitu",
+                        "Toadette", "King Boo", "Petey Piranha", "Baby Mario", "Baby Luigi", 
+                        "Baby Peach", "Baby Daisy", "Baby Rosalina", "Gold Mario", "Metal Mario",
+                        "Pink Gold Peach", "Wiggler", "Wario", "Waluigi", "Donkey Kong", "Bowser",
+                        "Dry Bones", "Bowser Jr.", "Dry Bowser", "Kamek", "Lemmy", "Larry", "Wendy",
+                        "Ludwig", "Iggy", "Roy", "Morton", "Peachette", "Inkling Girl", "Inkling Boy",
+                        "Villager", "Isabelle", "Link", "Diddy Kong", "Funky Kong", "Pauline",
+                        "Mii Normal Suit", "Mii Castle Suit", "Mii Dry Bowser Suit", "Mii ? Block Suit",
+                        "Mii Toadette Suit", "Mii Chain Chomp Suit", "Mii Cheep Cheep Suit", 
+                        "Mii Pokey Suit", "Mii Koopa Clown Car Suit", "Mii Dolphin Suit",  
+                        "Mii Blooper Suit", "Mii Para-Biddybud Suit", "Mii Ice Cream Suit", 
+                        "Mii Petey Piranha Suit", "Mii Goomba Suit", "Mii Birdo Suit", "Mii Wiggler Suit",
+                        "Mii Moo Moo Suit"];
+                        
 const insertKart = ["Standard Kart", "Pipe Frame", "Mach 8", "Steel Driver", "Cat Cruiser", 
                     "Circuit Special", "Tri-Speeder", "Badwagon", "Prancer", "Biddybuggy",
                     "Landship", "Sneeker", "Sports Coupe", "Gold Standard", "GLA",
